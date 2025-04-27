8c07a09bcba285c6754c6bf17293a2d9e3e33511
WhiteJadeWolf <shuvrangshubarua@gmail.com>
2025-04-27 06:35:47 +0530

    allowed all hosts

--- TrueDo/TrueDo/settings.py
+++ TrueDo/TrueDo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wz)=5c#vn37#xs^l7))^-$q0ftpdebrjr#82(c@+tj%&0d^)*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
